a600f754a2856f5b149a00d8c7b4f8172e3cd3c7
Rafael Camilo Tejón Rojas <rc.tejon@uniandes.edu.co>
2019-09-08 16:02:04 -0500

    Punto 1 parte 1 y 3

--- Lab3/Punto1.py
+++ Lab3/Punto1.py
@@ -5,6 +5,11 @@ import os, glob, pydicom
 import requests
 import skimage.io as io
 
+#
+# Punto 7.1
+#
+print('Inicia punto 7.1')
+
 #Descargar imagenes a color de internet
 image_url = "https://i0.wp.com/ascienceenthusiast.com/wp-content/uploads/2019/02/Eminem001A.jpg?w=605&ssl=1"
 r = requests.get(image_url)
@@ -18,3 +23,23 @@ with open("eminem2.jpg", "wb") as f:
 #Cargar las imagenes en la variables eminem1 y eminem2
 eminem1 = io.imread(os.path.join("eminem1.jpg"))
 eminem2 = io.imread(os.path.join("eminem2.jpg"))
+
+# Calculo de suma de imagenes variando alpha 
+alpha=0.
+while alpha<=1:
+    # Se realiza la suma de las imagenes y se convierte el tipo del arreglo a enteron de 8 bits sin signo
+    imagen=np.add((1-alpha)*eminem1,alpha*eminem2).astype(np.uint8)
+    plt.imshow(imagen)
+    alpha+=0.01
+    plt.title(f'Alpha: {alpha}')
+    plt.draw()
+    plt.pause(0.0001)
+    plt.clf()
+
+input("Press Enter to continue...")
+
+# Calculo promedio aritmetico
+imagen=np.add(0.5*eminem1,0.5*eminem2).astype(np.uint8)
+plt.imshow(imagen)
+plt.show()
+input("Press Enter to continue...")
