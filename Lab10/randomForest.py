5ce04bb2f8d2e5d83e20ce6f0f9ced7c8a289838
rctejon <rc.tejon@uniandes.edu.co>
2019-11-25 15:01:37 -0500

    Random Forest

--- Lab10/randomForest.py
+++ Lab10/randomForest.py
@@ -7,9 +7,23 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn import metrics
 from skimage import exposure
 from tqdm import tqdm
+from sklearn.externals import joblib
+
 
 #Carga de informacion
 data = scipyio.loadmat('mnist_dataset.mat')
+filename = 'random_forest3.sav'
+numArboles=50
+alturaArbol=None
+orienta=8
+cellSize=(4,4)
+
+loaded_model=None
+classifier=None
+try:
+    loaded_model = joblib.load(filename)
+except FileNotFoundError:
+    pass
 
 train_data = data['train_data']
 train_labels = data['train_labels'][0]
@@ -19,25 +33,31 @@ test_labels = data['test_labels'][0]
 train_data_hogs=[]
 test_data_hogs=[]
 
-for image in tqdm(train_data):
-    fd, hog_image = hog(image, orientations=8, pixels_per_cell=(7, 7),
-                        cells_per_block=(1, 1), visualize=True)
-    train_data_hogs.append(fd)
+if loaded_model==None:
+    for image in tqdm(train_data):
+        fd, hog_image = hog(image, orientations=orienta, pixels_per_cell=cellSize,
+                            cells_per_block=(1, 1), visualize=True)
+        train_data_hogs.append(fd)
 
-train_data_hogs = np.array(train_data_hogs)
-classifier = RandomForestClassifier(n_estimators=10)
+    train_data_hogs = np.array(train_data_hogs)
+    classifier = RandomForestClassifier(n_estimators=numArboles,max_depth=alturaArbol)
 
-classifier.fit(train_data_hogs, train_labels)
-print('Fin Fit')
+    classifier.fit(train_data_hogs, train_labels)
+    print('Fin Fit')
+
+if classifier!=None:
+    joblib.dump(classifier, filename)
 
 for image in tqdm(test_data):
-    fd, hog_image = hog(image, orientations=8, pixels_per_cell=(7, 7),
+    fd, hog_image = hog(image, orientations=orienta, pixels_per_cell=cellSize,
                         cells_per_block=(1, 1), visualize=True)
     test_data_hogs.append(fd)
 
 test_data_hogs = np.array(test_data_hogs)
 
-pred_rf = classifier.predict(test_data_hogs) 
+
+loaded_model = joblib.load(filename)
+pred_rf = loaded_model.predict(test_data_hogs) 
 print('Fin Pred')
 
 print(np.vstack((test_labels[0:10],pred_rf[0:10])))
